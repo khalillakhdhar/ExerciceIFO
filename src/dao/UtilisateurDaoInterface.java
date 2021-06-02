cb2b20f8ed7a64c88feb2d448a4c195aac89a32f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-02 14:33:44 +0100

    UserCrud

--- src/dao/UtilisateurDaoInterface.java
+++ src/dao/UtilisateurDaoInterface.java
@@ -1,10 +1,11 @@
 package dao;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import classes.Utilisateur;
 
 public interface UtilisateurDaoInterface {
-public ResultSet authentifier(String login,String mdp);
+public boolean authentifier(String login,String mdp) throws SQLException;
 public void adduser(Utilisateur us);
 }
